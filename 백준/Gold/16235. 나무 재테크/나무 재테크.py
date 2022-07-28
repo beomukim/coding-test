e445b15071271ca09796ee4bbdf9bc6486619075
김범우 <86512449+beomukim@users.noreply.github.com>
2022-07-29 03:50:53 +0900

    [Gold III] Title: 나무 재테크, Time: 852 ms, Memory: 179624 KB -BaekjoonHub

--- 백준/Gold/16235. 나무 재테크/나무 재테크.py
+++ 백준/Gold/16235. 나무 재테크/나무 재테크.py
@@ -1,60 +1,45 @@
 from collections import deque
-
-n, m, k = map(int, input().split(' '))
-
-a = [list(map(int, input().split(' '))) for _ in range(n)]
-graph = [[5] * n for _ in range(n)]
-trees = [[deque() for _ in range(n)] for _ in range(n)]
-dead_trees = [[list() for _ in range(n)] for _ in range(n)]
-for _ in range(m):
-    x, y, z = map(int, input().split(' '))
-    trees[x - 1][y - 1].append(z)
-
-dx = [-1, -1, 0, 1, 1, 1, 0, -1]
-dy = [0, 1, 1, 1, 0, -1, -1, -1]
-
-
-def spring_summer():
+import sys
+input = sys.stdin.readline
+def spring():
     for i in range(n):
         for j in range(n):
-            len_ = len(trees[i][j])
-            for k in range(len_):
-                if graph[i][j] < trees[i][j][k]:
-                    for _ in range(k, len_):
-                        dead_trees[i][j].append(trees[i][j].pop())
-                    break
+            len_t = len(t[i][j])
+            for k in range(len_t):
+                if t[i][j][k] <= no[i][j]:
+                    no[i][j] -= t[i][j][k]
+                    t[i][j][k] += 1
                 else:
-                    graph[i][j] -= trees[i][j][k]
-                    trees[i][j][k] += 1
-
+                    for _ in range(k, len_t):
+                        no[i][j] += t[i][j].pop() // 2
+                    break
+def fall():
     for i in range(n):
         for j in range(n):
-            while dead_trees[i][j]:
-                graph[i][j] += dead_trees[i][j].pop() // 2
-
-
-def fall_winter():
-    for i in range(n):
-        for j in range(n):
-            for k in range(len(trees[i][j])):
-                if trees[i][j][k] % 5 == 0:
+            for k in t[i][j]:
+                if k % 5 == 0:
                     for l in range(8):
-                        nx, ny, = i + dx[l], j + dy[l]
-
-                        if nx < 0 or nx >= n or ny < 0 or ny >= n:
-                            continue
-                        trees[nx][ny].appendleft(1)
-
-            graph[i][j] += a[i][j]
-
-
+                        x = i + dx[l]
+                        y = j + dy[l]
+                        if 0 <= x < n and 0 <= y < n:
+                            t[x][y].appendleft(1)
+            no[i][j] += s[i][j]
+dx = [1, -1, 0, 0, 1, -1, 1, -1]
+dy = [0, 0, -1, 1, 1, -1, -1, 1]
+n, m, k = map(int, input().split())
+s = []
+t = [[deque() for i in range(n)] for i in range(n)]
+no = [[5] * n for i in range(n)]
+for i in range(n):
+    s.append(list(map(int, input().split())))
+for i in range(m):
+    x, y, z = map(int, input().split())
+    t[x - 1][y - 1].append(z)
 for i in range(k):
-    spring_summer()
-    fall_winter()
-
-answer = 0
+    spring()
+    fall()
+cnt = 0
 for i in range(n):
     for j in range(n):
-        answer += len(trees[i][j])
-
-print(answer)
+        cnt += len(t[i][j])
+print(cnt)
