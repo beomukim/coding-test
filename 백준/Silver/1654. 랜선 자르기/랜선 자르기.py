0b7e01d7072603fe28b75f5a57be6e7b9c8629f4
김범우 <86512449+beomukim@users.noreply.github.com>
2022-08-02 02:51:19 +0900

    [Silver II] Title: 랜선 자르기, Time: 156 ms, Memory: 115768 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -3,21 +3,19 @@ lope = []
 for _ in range(k):
     lope.append(int(input()))
 
-lt = 0
+lt = 1
 rt = max(lope)
 
 while lt<=rt:
     mid = (lt+rt)//2
-    if mid == 0:
-        print(1)
-        exit(0)
     tmp = 0
     for i in lope:
         tmp += i // mid
 
-    if tmp < n:
-        rt = mid - 1
+    if tmp >= n:
+        res = mid
+        lt = mid+1
     else:
-        lt = mid + 1
+        rt = mid-1
 
-print(rt)
+print(res)
