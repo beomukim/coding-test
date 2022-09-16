cc0bad137d8baf2356a227044496b51ba0a537b6
김범우 <86512449+beomukim@users.noreply.github.com>
2022-09-17 01:04:45 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 71 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean solution(int x) {
+        int num = x;
+        int sum = 0;
+        while (x > 0) {
+            sum += x % 10;
+            x /= 10;
+        }
+
+        if (num % sum == 0) {
+            return true;
+        } else {
+            return false;
+        } 
+    }
+}
